c608a584e1dd894706b0292988e6e2e872c0e2a8
Fred Rotbart <rotbart@softbart.com>
2012-09-03 23:55:33 +1000

    Fix to punctuation in writer spec

--- spec/frame_writer_spec.rb
+++ spec/frame_writer_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 describe SrtTranslator::FrameWriter do
 	before :each do
-		frame = SrtTranslator::Frame.new("580", "00:39:52,725 --> 00:39:54,018", ['?רק קיטשי מספיק','טוב -'])
+		frame = SrtTranslator::Frame.new("580", "00:39:52,725 --> 00:39:54,018", ['רק קיטשי מספיק?','- טוב'])
 		@frames = []
 		@frames << frame << frame
 	end
@@ -14,13 +14,13 @@ describe SrtTranslator::FrameWriter do
 		SrtTranslator::FrameWriter.new(@frames, "some/path", testStream).write_frames
 		testStream.string.should == "580
 00:39:52,725 --> 00:39:54,018
-?רק קיטשי מספיק
-טוב -
+רק קיטשי מספיק?
+- טוב
 
 580
 00:39:52,725 --> 00:39:54,018
-?רק קיטשי מספיק
-טוב -
+רק קיטשי מספיק?
+- טוב
 
 "
 	end
